4a85c7c8f0c82b89002c6ad4a37b492f03ba59a3
Clayton <claytontey@gmail.com>
2021-10-25 14:50:34 -0300

    update aula4

--- src/aula3/conta.py
+++ src/aula3/conta.py
@@ -2,6 +2,7 @@ import datetime
 
 
 class Historico:
+    "Essa classe representa informações sobre histórico de transações"
     def __init__(self) -> None:
         self.data_abertura = datetime.datetime.today() 
         self.transacoes = []
@@ -15,15 +16,15 @@ class Historico:
 
 
 class Cliente:
+    "Essa classe representa informações sobre os dados dos clientes"
     def __init__(self,nome, sobrenome, cpf) -> None:
         self.nome = nome
         self.sobrenome = sobrenome
         self.cpf = cpf
     
 
-
-
 class Conta:
+    "Essa classe representa informações sobre as contas dos clientes"
     def __init__(self, numero, cliente, saldo, limite=1000.0) -> None:
         #print('Inicializando a minha conta')
         self.numero = numero
@@ -50,6 +51,15 @@ class Conta:
         print("numero: {} \nsaldo: {} \n".format(self.numero, self.saldo))
 
     def transfere_para(self, destino, valor):
+        """ Efetua uma transferência entre contas
+
+            Args:
+                destino (float): Conta destino.
+                valor (float): Valor a ser transferido
+
+            Returns:
+                bool: Um Booleano informando se a transação aconteceu ou não 
+        """
         retirou = self.saca(valor)
         if (retirou == False):
             return False
@@ -58,3 +68,4 @@ class Conta:
             self.historico.transacoes.append("Transferência de {} para conta {}".format(valor, destino.numero))
             return True
 
+   
